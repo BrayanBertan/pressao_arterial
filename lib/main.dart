0b76bd699cc0a4c72a329fab5e6cafd3df842e81
BrayanBertan <brayanbertan@gmail.com>
2020-05-11 10:55:36 -0300

    Testando

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'app/app_module.dart';
-
+//testando
 void main() {
   initializeDateFormatting().then((_) => runApp(ModularApp(module: AppModule())));
 
