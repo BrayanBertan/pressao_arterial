39297adc16498064cea2f570acc497524b13f0fa
BrayanBertan <brayanbertan@gmail.com>
2020-05-25 17:15:14 -0300

    isso vai dar uma merda

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,13 @@ import 'package:intl/intl.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'app/app_module.dart';
+import 'package:flutter_launcher_icons/android.dart';
+import 'package:flutter_launcher_icons/constants.dart';
+import 'package:flutter_launcher_icons/custom_exceptions.dart';
+import 'package:flutter_launcher_icons/ios.dart';
+import 'package:flutter_launcher_icons/main.dart';
+import 'package:flutter_launcher_icons/utils.dart';
+import 'package:flutter_launcher_icons/xml_templates.dart';
 //testando
 void main() {
   initializeDateFormatting('pt_BR', null).then((_) => runApp(ModularApp(module: AppModule())));
